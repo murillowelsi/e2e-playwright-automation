57e0b7b0ff8a584971f395882e81095ae16b7531
Murillo Welsi <murillo.welsi@gmail.com>
2022-06-30 08:54:37 +0100

    bypassing login with  localStorage

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,21 +1,11 @@
 import { test } from "@playwright/test";
 
-import { LoginPage } from "../pages/login/login-page";
 import { InventoryPage } from "../pages/inventory/inventory-page";
 
-let loginPage: LoginPage;
-let inventoryPage: InventoryPage;
-
-test.beforeEach(async ({ page }) => {
-  loginPage = new LoginPage(page);
-  inventoryPage = new InventoryPage(page);
-});
-
-test.beforeEach(async ({ page }) => {
-  await loginPage.go()
-  await loginPage.sigIn("standard_user", "secret_sauce");
-});
-
-test("should access the inventory page", async ({ page }) => {
+test("should access the inventory page", async ({ browser }) => {
+  const context = await browser.newContext({ storageState: "auth.json" });
+  const page = await context.newPage();
+  const inventoryPage = new InventoryPage(page);
+  
   await inventoryPage.go();
 });
